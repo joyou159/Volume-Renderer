cd6781cd82adf122c3209c0db1301a6f26c9d248
Youssef Ahmed <ya0721435@gmail.com>
2024-01-01 05:31:52 +0200

    reference axes and update bug is left

---
+++ VolumeRenderer.py
@@ -0,0 +1,150 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QFileDialog, QWidget, QMessageBox
+from PyQt5.QtCore import QTimer
+from PyQt5.uic import loadUi
+import vtk
+import os
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+import numpy as np
+
+
+class VolumeRenderer:
+    def __init__(self, vtk_widget, main_window):
+        self.vtk_widget = vtk_widget
+        self.main_window = main_window
+        self.volume = None
+        self.iso_value = 0
+        self.intensity_values = list()
+        self.visualize_flag = 0  # means nothing is visualized
+        self.main_window.ui.IsoValueSlider.valueChanged.connect(
+            self.handle_iso_value)
+        self.renderer = vtk.vtkRenderer()
+        self.render_window = vtk_widget.GetRenderWindow()
+        self.render_window.AddRenderer(self.renderer)
+        headlight = vtk.vtkLight()
+        headlight.SetLightTypeToHeadlight()
+        self.renderer.AddLight(headlight)
+
+        # Set up the camera
+        self.camera = self.renderer.GetActiveCamera()
+        self.camera.SetPosition(0, 0, 300)
+        self.camera.SetFocalPoint(0, 0, 0)
+
+        # Additional initialization if needed
+    def compute_intensity_values(self):
+        scalar_range = self.volume.GetOutput().GetScalarRange()
+        self.intensity_values = list(
+            range(int(scalar_range[0]), int(scalar_range[1]) + 1))
+        self.set_iso_sliders()
+
+    def apply_gaussian_smoothing(self, volume):
+        gaussian_smooth = vtk.vtkImageGaussianSmooth()
+        gaussian_smooth.SetInputData(volume)  # Set the input directly
+        gaussian_smooth.SetStandardDeviation(1.0)
+        gaussian_smooth.Update()
+        return gaussian_smooth.GetOutput()
+
+    def create_volume_actor(self, mapper, volume_property):
+        volume_actor = vtk.vtkVolume()
+        volume_actor.SetMapper(mapper)
+        volume_actor.SetProperty(volume_property)
+        return volume_actor
+
+    def add_reference_axes(self):
+        axes = vtk.vtkAxesActor()
+        widget = vtk.vtkOrientationMarkerWidget()
+        widget.SetOutlineColor(0.9300, 0.5700, 0.1300)
+        widget.SetOrientationMarker(axes)
+        widget.SetInteractor(self.vtk_widget)
+        widget.SetViewport(0.0, 0.0, 0.2, 0.2)
+        widget.SetEnabled(1)
+
+    def ray_casting_rendering(self, volume):
+
+        ray_cast_mapper = vtk.vtkGPUVolumeRayCastMapper()
+        ray_cast_mapper.SetInputData(volume)
+        ray_cast_mapper.SetBlendModeToComposite()
+
+        volume_property = vtk.vtkVolumeProperty()
+        volume_property.SetIndependentComponents(1)
+        volume_property.ShadeOn()
+
+        color_transfer_function = vtk.vtkColorTransferFunction()
+        color_transfer_function.AddRGBPoint(0, 0.0, 0.0, 0.0)
+        color_transfer_function.AddRGBPoint(200, 1.0, 1.0, 1.0)
+        volume_property.SetColor(color_transfer_function)
+
+        opacity_transfer_function = vtk.vtkPiecewiseFunction()
+        opacity_transfer_function.AddPoint(0, 0.0)
+        opacity_transfer_function.AddPoint(80, 0.1)
+        opacity_transfer_function.AddPoint(120, 0.8)
+        opacity_transfer_function.AddPoint(255, 1.0)
+        volume_property.SetScalarOpacity(opacity_transfer_function)
+
+        smoothed_volume = self.apply_gaussian_smoothing(volume)
+
+        ray_cast_mapper.SetInputData(smoothed_volume)
+
+        volume_actor = self.create_volume_actor(
+            ray_cast_mapper, volume_property)
+
+        self.renderer.AddVolume(volume_actor)
+
+        self.renderer.SetBackground(0, 0, 0)
+        volume_property.ShadeOn()
+        ray_cast_mapper.SetUseJittering(True)
+        ray_cast_mapper.SetSampleDistance(0.1)
+
+        self.renderer.ResetCamera()
+        self.add_reference_axes()
+
+    def update_visualization(self):
+        if self.volume:
+            current_position = self.camera.GetPosition()
+            current_focal_point = self.camera.GetFocalPoint()
+            current_zoom = self.camera.GetDistance()
+            self.renderer.RemoveAllViewProps()
+            # Check the rendering mode and update the visualization accordingly
+            if self.main_window.rendering_mode == 0:
+                # Surface Rendering
+                self.surface_rendering(self.volume.GetOutput(), self.iso_value)
+            elif self.main_window.rendering_mode == 1:
+                # Ray Casting Rendering
+                self.ray_casting_rendering(self.volume.GetOutput())
+            self.camera.SetPosition(current_position)
+            self.camera.SetFocalPoint(current_focal_point)
+            self.camera.SetDistance(current_zoom)
+            self.render_window.Render()
+
+    def handle_iso_value(self):
+        self.iso_value = self.main_window.ui.IsoValueSlider.value()
+        self.main_window.ui.IsoValue.setText(f"Iso Value: {self.iso_value}")
+        self.update_visualization()
+
+    def set_iso_sliders(self):
+        self.main_window.ui.IsoValueSlider.setMaximum(
+            self.intensity_values[-1])
+        self.main_window.ui.IsoValueSlider.setMinimum(
+            self.intensity_values[0])
+        self.main_window.ui.IsoValueSlider.setValue(
+            self.intensity_values[len(self.intensity_values)//2])
+
+    def surface_rendering(self, volume, iso_value):
+        smoothed_volume = self.apply_gaussian_smoothing(volume)
+
+        contour_filter = vtk.vtkContourFilter()
+        contour_filter.SetInputData(smoothed_volume)
+        contour_filter.GenerateValues(1, iso_value, iso_value)
+
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(contour_filter.GetOutputPort())
+
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+        actor.SetPosition(0, 0, 0)
+
+        self.renderer.AddActor(actor)
+
+        self.renderer.SetBackground(0, 0, 0)
+        self.renderer.ResetCamera()
+        self.add_reference_axes()
---
+++ main.py
@@ -0,0 +1,135 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QFileDialog, QWidget, QMessageBox
+from PyQt5.uic import loadUi
+import vtk
+import os
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+from VolumeRenderer import VolumeRenderer
+import copy
+
+
+class VTKMainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Load the main window UI
+        self.ui = loadUi('mainWindow.ui', self)
+        self.resize(1200, 800)
+        self.rendering_mode = None
+
+        # Set up the layout
+        layout = QVBoxLayout(self.ui.render_area)
+        self.ui.Import_button.clicked.connect(self.browse)
+        self.ui.clear_button.clicked.connect(self.clear_output)
+
+        self.vtk_widget = self.create_vtk_widget()
+        layout.addWidget(self.vtk_widget)
+
+        self.volume_renderer = VolumeRenderer(self.vtk_widget, self)
+        self.ui.IsoValue.setText(
+            f"Iso Value: {self.volume_renderer.iso_value}")
+
+        self.ui.SurfaceButton.toggled.connect(
+            self.handle_radio_button_toggled)
+        self.ui.RayCastButton.toggled.connect(
+            self.handle_radio_button_toggled)
+
+        # Set default radio button states
+        self.ui.SurfaceButton.setChecked(True)
+        # Initialize VolumeRenderer with the vtk_widget
+
+    def create_vtk_widget(self):
+        vtk_widget = QVTKRenderWindowInteractor(self.ui.render_area)
+        vtk_widget.GetRenderWindow().Render()
+        vtk_widget.Initialize()
+        return vtk_widget
+
+    def browse(self):
+        if self.volume_renderer.visualize_flag:
+            self.clear_output()
+        folder_path = QFileDialog.getExistingDirectory(
+            self, "Select DICOM Folder")
+
+        if not self.has_dicom_files(folder_path):
+            self.show_error_message("DICOM files not found.")
+            return
+        else:
+            dicom_reader = self.load_dicom_series(folder_path)
+            if dicom_reader:
+                self.volume_renderer.volume = dicom_reader
+                self.volume_renderer.compute_intensity_values()
+                if self.rendering_mode == 0:
+                    # Surface Rendering
+                    iso_value = self.volume_renderer.iso_value
+                    self.volume_renderer.surface_rendering(
+                        dicom_reader.GetOutput(), iso_value)
+                elif self.rendering_mode == 1:
+                    # Ray Casting Rendering
+                    self.volume_renderer.ray_casting_rendering(
+                        dicom_reader.GetOutput())
+
+    def show_error_message(self, message):
+        """
+        Displays an error message to the user.
+
+        Args:
+            message (str): The error message to be displayed.
+
+        Returns:
+            None
+        """
+        msg_box = QMessageBox()
+        msg_box.setIcon(QMessageBox.Icon.Critical)
+        msg_box.setWindowTitle("Error")
+        msg_box.setText(message)
+        msg_box.exec()
+
+    def load_dicom_series(self, directory):
+        reader = vtk.vtkDICOMImageReader()
+        reader.SetDirectoryName(directory)
+        reader.Update()
+        return reader
+
+    def has_dicom_files(self, directory):
+        if directory:
+            dicom_files = [file for file in os.listdir(
+                directory) if file.lower().endswith('.dcm')]
+            return bool(dicom_files)
+        else:
+            return False
+
+    def handle_radio_button_toggled(self):
+        """
+        Handle the toggling of the output radio buttons.
+
+        This function is called when the user toggles the output radio buttons.
+        It updates the `output` variable based on the selected radio button.
+
+        """
+        if self.ui.SurfaceButton.isChecked():
+            self.rendering_mode = 0
+            self.ui.IsoValueSlider.setEnabled(True)
+        elif self.ui.RayCastButton.isChecked():
+            self.rendering_mode = 1
+
+            self.ui.IsoValueSlider.setEnabled(False)
+
+        self.volume_renderer.update_visualization()
+
+    def clear_output(self):
+        self.volume_renderer = VolumeRenderer(self.vtk_widget, self)
+        self.ui.IsoValueSlider.setValue(0)
+        self.volume_renderer.render_window.Render()
+
+    def closeEvent(self, event):
+        # Explicitly clean up VTK resources when the window is closed
+        self.vtk_widget.GetRenderWindow().Finalize()
+        self.vtk_widget.GetRenderWindow().SetInteractor(None)
+        event.accept()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = VTKMainWindow()
+    window.show()
+    sys.exit(app.exec_())
